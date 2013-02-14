1fdfb479d8aea76a4348a6437731dcebf1b16362
Kurt Vangraefschepe <kvangrae@texeltek.com>
2013-02-14 17:52:50 -0500

    add RangeInputSplit to AccumuloInputFormat

--- src/main/java/org/apache/accumulo/core/client/mapreduce/AccumuloInputFormat.java
+++ src/main/java/org/apache/accumulo/core/client/mapreduce/AccumuloInputFormat.java
@@ -17,17 +17,58 @@
 package org.apache.accumulo.core.client.mapreduce;
 
 import com.texeltek.accumulocloudbaseshim.RecordReaderShim;
+import org.apache.accumulo.core.data.ByteSequence;
 import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
 
 public class AccumuloInputFormat extends InputFormatBase<Key, Value> {
 
     public RecordReader<Key, Value> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
         return new RecordReaderShim(impl.createRecordReader(split, context));
+    }
+
+    public static class RangeInputSplit extends InputSplit implements Writable {
+        public final cloudbase.core.client.mapreduce.CloudbaseInputFormat.RangeInputSplit impl;
+
+        public RangeInputSplit() {
+            this.impl = new cloudbase.core.client.mapreduce.CloudbaseInputFormat.RangeInputSplit();
+        }
+
+        public Range getRange() {
+            throw new UnsupportedOperationException();
+        }
+
+        public static float getProgress(ByteSequence start, ByteSequence end, ByteSequence position) {
+            return cloudbase.core.client.mapreduce.CloudbaseInputFormat.RangeInputSplit.getProgress(start.impl, end.impl, position.impl);
+        }
+
+        public float getProgress(Key currentKey) {
+            return impl.getProgress(currentKey.impl);
+        }
+
+        public long getLength() throws IOException {
+            return this.impl.getLength();
+        }
+
+        public String[] getLocations() throws IOException {
+            return impl.getLocations();
+        }
+
+        public void readFields(DataInput in) throws IOException {
+            impl.readFields(in);
+        }
+
+        public void write(DataOutput out) throws IOException {
+            impl.write(out);
+        }
     }
 }
