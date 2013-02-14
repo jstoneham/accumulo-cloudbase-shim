f6405f7284f5979340728043498718681d581a92
Kurt Vangraefschepe <kvangrae@texeltek.com>
2013-02-14 10:42:18 -0500

    add static methods to RangePartitioner

--- src/main/java/org/apache/accumulo/core/client/mapreduce/lib/partition/RangePartitioner.java
+++ src/main/java/org/apache/accumulo/core/client/mapreduce/lib/partition/RangePartitioner.java
@@ -2,9 +2,14 @@ package org.apache.accumulo.core.client.mapreduce.lib.partition;
 
 import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.Partitioner;
+
+import java.net.URI;
 
 public class RangePartitioner extends Partitioner<Text,Writable> implements Configurable {
 
@@ -25,5 +30,13 @@ public class RangePartitioner extends Partitioner<Text,Writable> implements Conf
     @Override
     public int getPartition(Text text, Writable writable, int i) {
         return impl.getPartition(text, writable, i);
+    }
+
+    public static void setSplitFile(JobContext job, String file) {
+        RangePartitioner.setSplitFile(job, file);
+    }
+
+    public static void setNumSubBins(JobContext job, int num) {
+        RangePartitioner.setNumSubBins(job, num);
     }
 }
